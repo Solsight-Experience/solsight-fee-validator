0bb770c2ea4d074050d216ffc68ab032ceebc991
Ngoc Le <ngocle266tqt@gmail.com>
2026-07-12 13:25:44 +0700

    fix: temp transactionsize

--- crates/lib/src/constant.rs
+++ crates/lib/src/constant.rs
@@ -7,7 +7,7 @@ pub const LAMPORTS_PER_SIGNATURE: u64 = 5000;
 pub const ESTIMATED_LAMPORTS_FOR_PAYMENT_INSTRUCTION: u64 = 50;
 pub const MIN_BALANCE_FOR_RENT_EXEMPTION: u64 = 2_039_280;
 pub const DEFAULT_INTEREST_MULTIPLIER: u128 = 100 * 24 * 60 * 60 / 10000 / (365 * 24 * 60 * 60);
-pub const MAX_TRANSACTION_SIZE: usize = 1232;
+pub const MAX_TRANSACTION_SIZE: usize = 1500;
 
 // HTTP Headers
 pub const X_RECAPTCHA_TOKEN: &str = "x-recaptcha-token";
